3fad0eddf97a47d68960b401f8d3658cc6f283b3
Mosharof <md5470415@gmail.com>
2024-04-24 16:03:45 +0600

    add all  task local storage and display Ui Done

--- js/app.js
+++ js/app.js
@@ -3,35 +3,41 @@ const inputField = document.getElementById('inputField');
 const addBtn = document.getElementById('addBtn');
 const listContainer = document.getElementById('listContainer');
 
-// get input field value function set
+// get input from user 
 function getUserInput() {
-    const inputValue = inputField.value;
+    const taskName = inputField.value;
     inputField.value = '';
-    return inputValue;
+    return taskName;
 }
-// keyboard enter event
-function handleEnterEvent(e) {
-    if (e.key === "Enter") {
-        inputValue = getUserInput();
-        if(!inputValue) return;
-        displayinputValue(inputValue);
+// handle add task 
+function handleAddTask(){
+    const taskName = getUserInput();
+        if(!taskName) return;
+        displayinputValue(taskName);
+        addTasktoLocalStorage(taskName)
+}
+// enter keyup event handler 
+function handleEnterEvent(e){
+    if(e.key === "Enter"){
+        handleAddTask()
     }
-
 }
 
 // click event hadler
-function handleClickEvent() {
-    const inputValue = getUserInput();
-    if(!inputValue) return;
-    displayinputValue(inputValue);
-}
+    // function clickEventHandler(){
+    //     const taskName = getUserInput();
+    //     if(!taskName) return;
+    //     displayinputValue(taskName);
+    //     addTasksTolocalStorage(taskName)
+    // }
+
 
 // display input value show
-function displayinputValue(inputValue) {
+function displayinputValue(taskName) {
     const li = document.createElement('li');
     li.innerHTML = `
-                <span id="inputvalue">${inputValue}</span>
-                <span id="action">
+                <span id="inputvalue">${taskName}</span>
+                <span style='display:flex;justify-content:space-around' id="action">
                     <button id="edit"><i style='font-size:20px'; class="fa-solid fa-pen-to-square"></i></button>
                     <button id="delete"><i  style='font-size:20px' class="fa-solid fa-delete-left"></i></button>
                 </span>
@@ -39,7 +45,39 @@ function displayinputValue(inputValue) {
     li.classList.add('listStyle');
     listContainer.appendChild(li)
 }
-// hadle events 
+// Step- 2: add task to localStorage
 
+// add tasks to localstorage
+function addTasksTolocalStorage(task){
+    localStorage.setItem("task", JSON.stringify(task))
+}
+// add task to localstorage
+function addTasktoLocalStorage(taskName){
+    const task = getTaskFromLocalStorage();
+    task.push(taskName);
+    addTasksTolocalStorage(task)
+}
+
+// get all task from localstorage
+function getTaskFromLocalStorage(){
+    let task = [];
+    const rowTask = localStorage.getItem('task');
+    if(rowTask){
+        task = JSON.parse(rowTask);
+    }
+    return task;
+}
+
+// display all task to UI when page loading
+function loadAndDisplayUiTask(){
+    const task = getTaskFromLocalStorage();
+    task.forEach(taskName =>  displayinputValue(taskName)
+    );
+}
+
+loadAndDisplayUiTask()
+// hadle events 
 inputField.addEventListener('keyup', handleEnterEvent);
-addBtn.addEventListener('click', handleClickEvent);
+addBtn.addEventListener('click', handleAddTask);
+
+
